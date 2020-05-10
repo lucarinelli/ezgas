19a7b5c4984fa031c771fb2c9d92d18789cd5081
Dario Licastro s234421 <s234421@studenti.polito.it>
2020-05-10 19:23:51 +0200

    src: New login and repository query

--- src/main/java/it/polito/ezgas/repository/UserRepository.java
+++ src/main/java/it/polito/ezgas/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package it.polito.ezgas.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import it.polito.ezgas.entity.User;
@@ -8,4 +9,7 @@ import it.polito.ezgas.entity.User;
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
 
+	  @Query(value = "SELECT * FROM USERS WHERE EMAIL_ADDRESS = ?0", nativeQuery = true)
+	  User findByEmailAddress(String emailAddress);
+	
 }
