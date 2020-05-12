9b7f5ce466ffc3e0fa051d8a8045e5789ce9e2e8
Ignacio Lopez-Perea Villacanas <s275625@studenti.polito.it>
2020-05-12 07:47:47 +0000

    src: resolve issue 36

--- src/main/java/it/polito/ezgas/repository/GasStationRepository.java
+++ src/main/java/it/polito/ezgas/repository/GasStationRepository.java
@@ -1,5 +1,7 @@
 package it.polito.ezgas.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,6 @@ import it.polito.ezgas.entity.GasStation;
 
 @Repository
 public interface GasStationRepository extends JpaRepository<GasStation, Integer> {
-
+    
+    List<GasStation> findByCarSharing(String carSharing);
 }
