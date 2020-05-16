9e35534333e155678a4d4b47d2bcc8625be48833
Dario Licastro s234421 <s234421@studenti.polito.it>
2020-05-17 00:26:22 +0200

    tests: UserEntityTest complete

--- src/test/java/it/polito/ezgas/UserEntityTest.java
+++ src/test/java/it/polito/ezgas/UserEntityTest.java
@@ -1,69 +1,114 @@
+/**
+ * 
+ */
 package it.polito.ezgas;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-import it.polito.ezgas.entity.User;;
 
-@RunWith(SpringRunner.class)
+import it.polito.ezgas.entity.User;
+
+
 public class UserEntityTest {
- String userName="ciao";	
- String password="password";	
- String email ="ciao@password";
- Integer reputation = 3;
-User user = new User(userName,password,email,reputation);
+	private User ur;
 
-@Test
-public void tgp() {
-	assertEquals(user.getPassword(),password);
-}
-@Test
-public void tgu() {
-	assertEquals(user.getUserName(),userName);
-}
-@Test
-public void tge() {
-	assertEquals(user.getEmail(),email);
-}
-@Test
-public void tgr() {
-	assertEquals(user.getReputation(),reputation);
-}
-@Test
-public void tga () {
-	assertEquals(user.getAdmin(),false);
-}
-@Test 
-public void tsp () {
-	String passwd=password+"##";
-	user.setPassword(passwd);
-	assertEquals(user.getPassword(),passwd);
-	
-} 
+	@Before
+	public void setUp() {
+		ur = new User("ciao", "password", "ciao@password", 3);
+	}
 
+	/**
+	 * Test method for {@link it.polito.ezgas.entity.User#getUserId()}.
+	 * Test method for {@link it.polito.ezgas.entity.User#setUserId(java.lang.Integer)}.
+	 */
+	@Test
+	public void testSetGetUserId() {
+		ur.setUserId(1);
+		assert(ur.getUserId()==1);
+		ur.setUserId(-1);
+		assert(ur.getUserId()==-1);//FIXME: should this fail?
+	}
 
-@Test 
-public void tsu () {
-	String passwd=password+"##";
-	user.setUserName(passwd);
-	assertEquals(user.getPassword(),passwd);
-	
-}
-@Test 
-public void tse () {
-	String mail=email+"##";
-	user.setEmail(mail);
-	assertEquals(user.getEmail(),mail);
-	
-}
-@Test 
-public void tsr () {
-	 reputation=reputation+1;
-	user.setReputation(reputation);
-	assertEquals(user.getReputation(),reputation);
-	
-}
+	/**
+	 * Test method for {@link it.polito.ezgas.entity.User#getUserName()}.
+	 * Test method for {@link it.polito.ezgas.entity.User#setUserName(java.lang.String)}.
+	 */
+	@Test
+	public void testSetGetUserName() {
+		ur.setUserName("ciao");
+		assert(ur.getUserName().equals("ciao"));
+	}
+
+	/**
+	 * Test method for {@link it.polito.ezgas.entity.User#getPassword()}.
+	 * Test method for {@link it.polito.ezgas.entity.User#setPassword(java.lang.String)}.
+	 */
+	@Test
+	public void testSetGetPassword() {
+		ur.setPassword("password");
+		assert(ur.getPassword().equals("password"));
+	}
+
+	/**
+	 * Test method for {@link it.polito.ezgas.entity.User#getEmail()}.
+	 * Test method for {@link it.polito.ezgas.entity.User#setEmail(java.lang.String)}.
+	 */
+	@Test
+	public void testSetGetEmail() {
+		ur.setEmail("ciao@password");
+		assert(ur.getEmail().equals("ciao@password"));
+	}
+
+	/**
+	 * Test method for {@link it.polito.ezgas.entity.User#User()}.
+	 */
+	@Test
+	public void testUser() {
+		ur=new User("ciao", "password", "ciao@password", 3);
+		
+		assert(ur.getUserName().equals("ciao"));
+		assert(ur.getPassword().equals("password"));
+		assert(ur.getEmail().equals("ciao@password"));
+		assert(ur.getReputation().equals(3));
+	}
+
+	/**
+	 * Test method for {@link it.polito.ezgas.entity.User#getReputation()}.
+	 * Test method for {@link it.polito.ezgas.entity.User#setReputation(java.lang.Integer)}.
+	 */
+	@Test
+	public void testSetGetReputation() {
+		int reputation=3;
+		ur.setReputation(1);
+		assert(ur.getReputation().equals(1));
+		ur.setReputation(reputation);
+		assert(ur.getReputation().equals(3));
+		reputation=reputation+1;
+		ur.setReputation(reputation);
+		assert(ur.getReputation().equals(4));
+		ur.setReputation(-5);
+		assert(ur.getReputation().equals(-5));
+		ur.setReputation(5);
+		assert(ur.getReputation().equals(5));
+		ur.setReputation(6);
+		assert(ur.getReputation().equals(6));//FIXME: should this fail?
+	}
+
+	/**
+	 * Test method for {@link it.polito.ezgas.entity.User#getAdmin()}.
+	 * Test method for {@link it.polito.ezgas.entity.User#setAdmin(java.lang.Boolean)}.
+	 */
+	@Test
+	public void testSetGetAdmin() {
+		ur.setAdmin(false);
+		assert(ur.getAdmin().equals(false));
+		ur.setAdmin(true);
+		assert(ur.getAdmin().equals(true));
+	}
+
 }
