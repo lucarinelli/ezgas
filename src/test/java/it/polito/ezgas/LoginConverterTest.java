b9c1505e1bc698b0eaa5480de1e7b844e786d546
nanol <s275625@polito.it>
2020-05-22 17:27:48 +0200

    src: LoginConverter Test case

---
+++ src/test/java/it/polito/ezgas/LoginConverterTest.java
@@ -0,0 +1,19 @@
+package it.polito.ezgas;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class LoginConverterTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
