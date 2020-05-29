9fe1dcf2f1733f3cc906cb730651ce9dce9c3798
Luca Rinelli <rinelliluca@gmail.com>
2020-05-29 19:34:03 +0200

    tests: templates + first gas station controller tests

---
+++ src/test/java/it/polito/ezgas/HomeControllerTest.java
@@ -0,0 +1,59 @@
+package it.polito.ezgas;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class HomeControllerTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public final void testAdmin() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testIndex() {
+		fail("Not yet implemented"); // TODO Luca
+	}
+
+	@Test
+	public final void testMap() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testLogin() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testUpdate() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testSignup() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
